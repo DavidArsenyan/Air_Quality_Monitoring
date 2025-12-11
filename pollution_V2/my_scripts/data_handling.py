4e00cc6842040fce772ca8a4eedf458105272879
DavidArsenyan <90145902+DavidArsenyan@users.noreply.github.com>
2025-12-11 16:07:29 +0400

    Update data_handling.py

--- pollution_V2/my_scripts/data_handling.py
+++ pollution_V2/my_scripts/data_handling.py
@@ -3,7 +3,7 @@ import pandas as pd
 from openaq import OpenAQ
 
 client = OpenAQ(api_key="f2ae9f923f46869d0254a8f714b115d8ff9b26ae25178ab506172346f487455b")
-def func(sensor_id):
+def func(sensor_id): # initilize function for getting data of sensors which correspond to the following conditions
     response = client.measurements.list(
     sensors_id=sensor_id,
     data="days",
@@ -15,8 +15,8 @@ def func(sensor_id):
 
 new_data = pd.read_csv("../data/my_sensors_with_dates.csv")
 sens_data = new_data["sensor_id"]
-
-for i in sens_data:
+ 
+for i in sens_data: # here we just create a new dataframe for the sensors and make a .csv file for them with this properties
     all_data = []
     response = func(i)
     for m in response.results:
